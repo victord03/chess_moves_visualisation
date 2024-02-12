b8585b3d3bff3e80f67b155e44aeba8161fe6435
victord03 <vic.kaklamanis@gmail.com>
2024-02-12 18:04:51 +0200

    Added a Board class.

--- src/main.py
+++ src/main.py
@@ -1,35 +1,4 @@
 
-"""
-
-movement_dictionary = {
-    'Pawn': set(),
-    'Rook': set(),
-    'Knight': set(),
-    'Bishop': set(),
-    'Queen': set(),
-    'King': set(),
-}
-
-
-Rook movement:
-    Any number of squares on the same file,
-    Any number of squares on the same rank.
-
-    current_position_rank = 'A'
-    current_position_file = '1'
-
-        ranks = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'}
-        files = {'1', '2', '3', '4', '5', '6', '7', '8'}
-
-        ranks.remove(current_position_rank)
-        files.remove(current_position_file)
-
-
-        zip(position_ranks, position_files)
-
-"""
-
-
 class BoardState:
     state: dict
 
@@ -40,21 +9,87 @@ class BoardState:
             'B1': '',
             'C1': '',
             'D1': '',
-            'E1': ''
+            'E1': '',
+            'F1': '',
+            'G1': '',
+            'H1': '',
+            'A2': '',
+            'B2': '',
+            'C2': '',
+            'D2': '',
+            'E2': '',
+            'F2': '',
+            'G2': '',
+            'H2': '',
+            'A3': '',
+            'B3': '',
+            'C3': '',
+            'D3': '',
+            'E3': '',
+            'F3': '',
+            'G3': '',
+            'H3': '',
+            'A4': '',
+            'B4': '',
+            'C4': '',
+            'D4': '',
+            'E4': '',
+            'F4': '',
+            'G4': '',
+            'H4': '',
+            'A5': '',
+            'B5': '',
+            'C5': '',
+            'D5': '',
+            'E5': '',
+            'F5': '',
+            'G5': '',
+            'H5': '',
+            'A6': '',
+            'B6': '',
+            'C6': '',
+            'D6': '',
+            'E6': '',
+            'F6': '',
+            'G6': '',
+            'H6': '',
+            'A7': '',
+            'B7': '',
+            'C7': '',
+            'D7': '',
+            'E7': '',
+            'F7': '',
+            'G7': '',
+            'H7': '',
+            'A8': '',
+            'B8': '',
+            'C8': '',
+            'D8': '',
+            'E8': '',
+            'F8': '',
+            'G8': '',
+            'H8': '',
 
         }
+
+    def __setitem__(self, key, value):
+        self.state[key] = value
 
 
 class ChessPiece:
     name: str
     colour: str
-    position: tuple
+    position: str
     # pawn_double_move: bool
 
-    def __init__(self, name, colour, position):
+    def __init__(self, name, colour, position, board):
         self.name = name
         self.colour = colour
         self.position = position
+        self.update_board_state(board)
+
+    def update_board_state(self, board: BoardState):
+        board[f'{self.position[0]}{self.position[1]}'] = self.name
 
     def rook_instructions(self) -> tuple:
         current_position_rank = self.position[0]
@@ -72,7 +107,7 @@ class ChessPiece:
                 continue
             all_legal_moves.append(f'{rank}{current_position_file}')
 
-        print(all_legal_moves)
+        # print(all_legal_moves)
         return tuple(all_legal_moves)
 
     def pawn_instructions(self):
@@ -92,13 +127,18 @@ def generate_legal_moves_for(piece: ChessPiece):
         return piece.rook_instructions()
 
 
-def visualise_moves_for(piece):
+def visualise_moves_for(piece: ChessPiece):
     ...
 
 
 def main():
-    rook = ChessPiece(name='Rook', colour='White', position=('A', '1'))
+    board = BoardState()
+
+    rook = ChessPiece(name='Rook', colour='White', position=('A', '1'), board=board)
     generate_legal_moves_for(rook)
+
+    # print(board.state)
+    # print(len(list(board.state.keys())))
 
 
 if __name__ == '__main__':
--- test/test_main.py
+++ test/test_main.py
@@ -11,22 +11,38 @@ from which the piece can capture back and end on the capture square.
 '''
 
 
-def setup():
+def setup_board():
+    return m.BoardState()
+
+
+def setup_piece():
+    board = setup_board()
     name = 'Rook'
     colour = 'White'
-    position = ('A', '1')
+    position = 'A1'
 
-    return m.ChessPiece(name, colour, position)
-
-
-def test_chess_piece():
-    assert isinstance(setup(), m.ChessPiece)
+    return board, m.ChessPiece(name=name, colour=colour, position=position, board=board)
 
 
-def test_generate_legal_moves_for():
-    rook = setup()
-    assert m.generate_legal_moves_for(rook) == ('A2', 'A3', 'A4', 'A5', 'A6', 'A7', 'A8', 'B1', 'C1', 'D1', 'E1', 'F1', 'G1', 'H1')
+class TestPiece:
+    def test_chess_piece(self):
+        assert isinstance(setup_piece()[1], m.ChessPiece)
+
+    def test_new_piece_board_update(self):
+        board, piece = setup_piece()
+        assert board.state == {'A1': 'Rook', 'B1': '', 'C1': '', 'D1': '', 'E1': '', 'F1': '', 'G1': '', 'H1': '', 'A2': '', 'B2': '', 'C2': '', 'D2': '', 'E2': '', 'F2': '', 'G2': '', 'H2': '', 'A3': '', 'B3': '', 'C3': '', 'D3': '', 'E3': '', 'F3': '', 'G3': '', 'H3': '', 'A4': '', 'B4': '', 'C4': '', 'D4': '', 'E4': '', 'F4': '', 'G4': '', 'H4': '', 'A5': '', 'B5': '', 'C5': '', 'D5': '', 'E5': '', 'F5': '', 'G5': '', 'H5': '', 'A6': '', 'B6': '', 'C6': '', 'D6': '', 'E6': '', 'F6': '', 'G6': '', 'H6': '', 'A7': '', 'B7': '', 'C7': '', 'D7': '', 'E7': '', 'F7': '', 'G7': '', 'H7': '', 'A8': '', 'B8': '', 'C8': '', 'D8': '', 'E8': '', 'F8': '', 'G8': '', 'H8': ''}
 
 
-def test_visualise_moves_for():
-    ...
+class TestVisualisationEngine:
+    def test_generate_legal_moves_for(self):
+        board, piece = setup_piece()
+        assert m.generate_legal_moves_for(piece) == ('A2', 'A3', 'A4', 'A5', 'A6', 'A7', 'A8', 'B1', 'C1', 'D1', 'E1', 'F1', 'G1', 'H1')
+
+    def test_visualise_moves_for(self):
+        ...
+
+
+class TestBoard:
+    def test_board_state(self):
+        assert len(list(setup_board().state.keys())) == 64
+
